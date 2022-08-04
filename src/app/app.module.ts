af901b2982f69ccde5330d36b11a3daec1138b05
Trend20 <enockomondi305@gmail.com>
2022-08-05 00:03:37 +0300

    adding test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WaringalertComponent } from './waringalert/waringalert.component';
+import { SuccessalertComponent } from './successalert/successalert.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WaringalertComponent,
+    SuccessalertComponent,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/waringalert/waringalert.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WaringalertComponent } from './waringalert.component';
+
+describe('WaringalertComponent', () => {
+  let component: WaringalertComponent;
+  let fixture: ComponentFixture<WaringalertComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WaringalertComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(WaringalertComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
