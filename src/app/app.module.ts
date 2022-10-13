8c2515fe2fb1f4aa24c1bb12f75343a0123aa4f6
Trend20 <enockomondi305@gmail.com>
2022-10-13 03:21:42 +0300

    recipe-book component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent
+    ShoppingListComponent,
+    RecipeBookComponent
   ],
   imports: [
     BrowserModule,
