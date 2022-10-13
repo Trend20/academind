1916818abcda117fce4948a0076e9622f7abc09c
Trend20 <enockomondi305@gmail.com>
2022-10-13 03:24:58 +0300

    adding sub-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeBookComponent } from './recipe-book/recipe-book.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { ListEditComponent } from './shopping-list/list-edit/list-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    RecipeBookComponent
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ListEditComponent
   ],
   imports: [
     BrowserModule,
