91bd5f210799de57a8b9287e0ccca0c488b07602
Trend20 <enockomondi305@gmail.com>
2022-10-13 03:20:41 +0300

    shopping list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule,
